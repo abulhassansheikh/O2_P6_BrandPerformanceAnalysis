e2eabd15c0f7cc4e85cca36ff868bef14256f5e1
Abul Hassan Sheikh <asheikh@tdotperformance.ca>
2019-07-22 17:49:28 -0400

    July 22 2019, early data wrangling

--- O2_P8_BrandPerformanceAnalysis_PR_M_BrandPulseDashboard.py
+++ O2_P8_BrandPerformanceAnalysis_PR_M_BrandPulseDashboard.py
@@ -1,20 +1,16 @@
 
 ###Brand Pulse Dashboard
 import pandas as pd
-from dfply import *
 
 PastYearData =pd.read_csv("//192.168.2.32/Group/Data Team/Brand_Update_Location/11_Brand_Performance_Analysis/PastYearDataNew.csv", encoding='utf-8')	
 CurrentYearData =pd.read_csv("//192.168.2.32/Group/Data Team/Brand_Update_Location/11_Brand_Performance_Analysis/CurrentYearDataNew.csv", encoding='utf-8')	
 
 SD = pd.concat([PastYearData, CurrentYearData])
 
-SD[(SD['Order_Date']!="") & (SD['Supplier']=="Keystone")].groupby('attribute_set')
-
-(SD >>
-     group_by(X.attribute_set)>>
-     summarize(Supplier = X.Supplier.mean))
-
-SD >> select("Order_Date", "Order_Number") >> head(3)
-
 SD.columns
-SD["Supplier"]
+
+SD[(SD['Order_Date']!="") & (SD['Supplier']=="Keystone")].groupby('attribute_set').count()
+
+test = SD.groupby("attribute_set", as_index=False)['Order_Date'].agg({"count":"count"}).sort_values("count", ascending = False)
+
+test.index
