ea218f393f9e7773c2d0f34cd46fa5695d3dbfa8
Abul Hassan Sheikh <asheikh@tdotperformance.ca>
2019-07-23 15:34:33 -0400

    Completed: Number of items sold per brand per year

--- O2_P8_BrandPerformanceAnalysis_PR_M_BrandPulseDashboard.py
+++ O2_P8_BrandPerformanceAnalysis_PR_M_BrandPulseDashboard.py
@@ -6,28 +6,52 @@ import pandas as pd
 PastYearData =pd.read_csv("//192.168.2.32/Group/Data Team/Brand_Update_Location/11_Brand_Performance_Analysis/PastYearDataNew.csv", encoding='utf-8')	
 CurrentYearData =pd.read_csv("//192.168.2.32/Group/Data Team/Brand_Update_Location/11_Brand_Performance_Analysis/CurrentYearDataNew.csv", encoding='utf-8')	
 
-SD = pd.concat([PastYearData, CurrentYearData])
+#Combine Past and recent years sales data
+SD = pd.concat([PastYearData, CurrentYearData], ignore_index=True)
 
-SD.columns
-SD['Order_Date']
+#Convert Order_Date string to date and extract relevant date values
+SD['Order_Date'] = pd.to_datetime(SD['Order_Date'], format= "%d-%b-%y")
+SD['OD_Year'] = SD['Order_Date'].dt.strftime('%Y')
+SD['OD_MonthNum'] = SD['Order_Date'].dt.strftime('%m')
+SD['OD_MonthLab'] = SD['Order_Date'].dt.strftime('%B')
+SD['OD_MonthDay'] = SD['Order_Date'].dt.strftime('%d')
+SD['OD_WeekDay'] = SD['Order_Date'].dt.strftime('%A')
 
-test = SD
-
-test['Order_Date'] = pd.to_datetime(test['Order_Date'], format= "%Y-%m-%d")
-test.dtypes
-
-test[test['Order_Date'] > '2018-01-01'] 
-
-
-FilterSD =    SD[(SD['Order_Date']!="") 
+#Filter out any blank orderdate values
+FilterSD =    SD[(SD['Order_Date']!="")
            #& (SD['Supplier']=="Keystone")
            ]
 
-GroupedSD = (FilterSD.groupby("attribute_set", as_index=False)
+#Group by Attribute set & year sold, then count number of sales
+GroupedSD = (FilterSD.groupby(["attribute_set","OD_Year"], as_index=False)
                 ['Order_Date'].
-                agg({"count":"count"}).sort_values("count", ascending = False))
+                agg({"count":"count"}).sort_values(["attribute_set","OD_Year"] , ascending = True))
 
 
-test['Year'] = round(test['Order_Date'].dt.year,0)
-test['Month'] = test['Order_Date'].dt.month
-df['Day'] = df['Date'].dt.day
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
